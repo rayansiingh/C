64a40e34a9540ae7354265f76ed79eb5b40a333b
Rayan Singh <144525324+rayansiingh@users.noreply.github.com>
2024-02-24 14:32:08 +0530

    Delete extension cyber directory

--- extension cyber/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message && message.action === "displayURL" && message.url) {
-      // Message received from content script, do something with the URL
-      console.log("Received URL:", message.url);
-      // Send a response if necessary
-      sendResponse({ received: true });
-  }
-});
--- extension cyber/content.js
+++
@@ -1,3 +0,0 @@
-chrome.runtime.sendMessage({ action: "displayURL", url: window.location.href }, function(response) {
-  console.log("Response received:", response);
-});
